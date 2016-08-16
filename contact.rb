d5223f060e02c1bf4b378f40ce0126e855e2eaa3
Max Maung <maung@outlook.com>
2016-08-16 16:23:07 -0400

    working copy minus one option

--- contact.rb
+++ contact.rb
@@ -1,32 +1,61 @@
 class Contact
 
+  @@contacts = []
+  @@ids = 0
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :note
   # This method should initialize the contact's attributes
-  def initialize
+  def initialize (first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @@ids += 1
+    @id = @@ids
+    @@contacts << self
 
   end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name,last_name,email,note)
+    return new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
-
+    @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
+  def self.find(id)
+    @@contacts.each do |contact|
+      if id == contact.id
+        return contact
+      end
+    end
 
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
-  def update
+  def update (attribute, new_value)
+
+      if attribute == "First_Name"
+        @first_name = new_value
+      elsif attribute == "Last_Name"
+        @last_name = new_value
+      elsif attribute == "Email"
+        @email = new_value
+      elsif attribute == "Note"
+        @note = new_value
+      else
+        puts "Invalid Entry"
+    end
 
   end
 
@@ -40,19 +69,20 @@ class Contact
 
   # This method should delete all of the contacts
   def self.delete_all
-
+      @@contacts.delete
   end
 
   def full_name
-
+    "#{first_name} #{last_name}"
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
+      @@contacts.delete(self)
 
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
 end
--- crm.rb
+++ crm.rb
@@ -1,3 +1,5 @@
+require_relative 'contact'
+
 class CRM
 
   def initialize
@@ -5,46 +7,114 @@ class CRM
   end
 
   def main_menu
-
+    while true # repeat indefinitely
+    print_main_menu
+    user_selected = gets.to_i
+    call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+  puts '[1] Add a new contact'
+  puts '[2] Modify an existing contact'
+  puts '[3] Delete a contact'
+  puts '[4] Display all the contacts'
+  puts '[5] Search by attribute'
+  puts '[6] Exit'
+  puts 'Enter a number:'
   end
 
-  def call_option
-
+  def call_option(user_selected)
+    case user_selected
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+    when 3 then delete_contact
+    when 4 then display_all_contacts
+    when 5 then search_by_attribute
+    when 6 then exit
+    end
   end
 
   def add_new_contact
+
+  print 'Enter First Name: '
+  first_name = gets.chomp
+
+  print 'Enter Last Name: '
+  last_name = gets.chomp
+
+  print 'Enter Email Address: '
+  email = gets.chomp
+
+  print 'Enter a Note: '
+  note = gets.chomp
+
+  Contact.create(first_name, last_name, email, note)
 
   end
 
   def modify_existing_contact
 
+    display_all_contacts
+
+    puts "Please choose the ID for the contact you want to update"
+    id = gets.chomp.to_i
+
+    contact = Contact.find(id)
+
+    puts "Please choose what you want to update: First_Name, Last_Name, Email, Note"
+    attribute = gets.chomp
+
+    puts "Enter New Value"
+    new_value = gets.chomp
+    contact.update(attribute,new_value)
+
   end
 
   def delete_contact
+    displaycontact = Contact.all
+    displaycontact.each do |contact|
+      puts "ID: #{contact.id} FirstName:#{contact.first_name} LastName:#{contact.last_name} E-Mail:#{contact.email} Note:#{contact.note}"
+    end
+    puts "Enter ID to delete"
+    answer = gets.chomp.to_i
+
+    contact = Contact.find (answer)
+    contact.delete
 
   end
 
   # This method should accept as an argument an array of contacts
   # and display each contact in that array
-  def display_contacts
-
+  def display_contacts(contact_list)
+    # puts "Enter ID for single contact or ALL for all contacts"
+    # answer = gets.chomp
+    # case answer
+    #
+    # when "ALL" then Contact.all
+    # when "ID" then Contact.find(answer)
+    # else "Invalid Option"
+    # end
+    contact_list.each do |contact|
+      puts "ID : #{contact.id} #{contact.full_name}, #{contact.email}, #{contact.note}"
+    end
     # HINT: Make use of this method in the display_all_contacts and search_by_attribute methods to keep your code DRY
   end
 
   def display_all_contacts
-    
+    display_contacts(Contact.all)
+
     # HINT: Make use of the display_contacts method to keep your code DRY
   end
 
   def search_by_attribute
-    
+
     # HINT: Make use of the display_contacts method to keep your code DRY
   end
 
   # Add other methods here, if you need them.
 
 end
+
+mycrm = CRM.new()
+mycrm.main_menu
